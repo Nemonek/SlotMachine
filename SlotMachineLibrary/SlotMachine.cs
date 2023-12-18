33b95d8867b9b46d5f2c38f5dcf355a453603607
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2023-12-18 19:09:14 +0100

    commit

--- SlotMachineLibrary/SlotMachine.cs
+++ SlotMachineLibrary/SlotMachine.cs
@@ -1,5 +1,86 @@
-﻿namespace SlotMachineLibrary;
+﻿
+using System.Runtime.CompilerServices;
+
+namespace SlotMachineLibrary;
 public class SlotMachine
 {
+    private char[] _lettere = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Z' };
 
+    private int _saldoGiocatore;
+
+    public int SaldoGiocatore { get => this._saldoGiocatore; private set => this._saldoGiocatore = value; }
+
+    private char[] _ultimoRoll;
+    public char[] UltimoRoll { get => this._ultimoRoll; private set => this._ultimoRoll = value; }
+
+    private int _counter;
+    public int Counter { get => this._counter;  }
+    public SlotMachine() {
+        this._saldoGiocatore = 0;
+        this._counter = 2;
+    }
+
+    public void AggiungiCredito(int n)
+    {
+        if (n <= 0) throw new();
+
+        this._saldoGiocatore += n;
+    }
+
+    public char[] EseguiRollMantenendo(int[] input)
+    {
+        Random r = new();
+
+        this._ultimoRoll[0] = (input[0] == -1) ? this._lettere[r.Next(0, 21)] : this._ultimoRoll[0];
+        this._ultimoRoll[1] = (input[1] == -1) ? this._lettere[r.Next(0, 21)] : this._ultimoRoll[1];
+        this._ultimoRoll[2] = (input[2] == -1) ? this._lettere[r.Next(0, 21)] : this._ultimoRoll[2];
+
+        this._counter++;
+
+        return this._ultimoRoll;
+    }
+
+    public char[] EseguiRoll()
+    {
+        this._saldoGiocatore--;
+        this._counter = 0;
+        Random r = new();
+        char[] roll = { this._lettere[r.Next(0, 21)], this._lettere[r.Next(0, 21)], this._lettere[r.Next(0, 21)] };
+        this._ultimoRoll = roll;
+        int premio = DeterminaPremio();
+
+        return this._ultimoRoll;
+    }
+
+    private int DeterminaPremio()
+    {
+        int check = new HashSet<char>(this._ultimoRoll).Count;
+        if (check == 1) return this._lettere.IndexOf(this._ultimoRoll[0]);
+        
+        if (check == 2) return 1;
+
+        else
+        { // check = 3
+            int n = this._lettere.IndexOf(this._ultimoRoll[0]);
+
+            if (this._ultimoRoll[0] == 'z' && this._ultimoRoll[1] == 'z' && this._ultimoRoll[2] == 'z') return 100;
+
+            if (n + 2 > 21) return 0;
+
+            if (this._lettere[n + 1] == this._ultimoRoll[1] && this._lettere[n + 2] == this._ultimoRoll[2]) return 50;
+            return 0;
+        }
+    }
+}
+
+public static class ArrayHelper
+{
+    public static int IndexOf(this char[] input, char c)
+    {
+        int l = input.Length;
+        for (int i = 0; i < l; i++)
+            if (c == input[i]) return i;
+
+        return -1;
+    }
 }
---
+++ WPFSlotMachine/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using SlotMachineLibrary;
+
+namespace WPFSlotMachine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SlotMachine m;
+        private int[] _tieni;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.m = new();
+            this._tieni = new int[3];
+        }
+
+        private void AggiungiCredito(object sender, RoutedEventArgs e)
+        {
+            int ToAdd =  int.Parse(QuantoCreditoAggiungere.Text);
+            m.AggiungiCredito(ToAdd);
+            Credito.Text = m.SaldoGiocatore.ToString();
+        }
+
+        private void RollaLettere(object sender, RoutedEventArgs e)
+        {
+            
+            char[] roll;
+            if (m.Counter < 2 && (this._tieni[0] != -1 || this._tieni[1] != -1 || this._tieni[2] != -1))
+                roll = m.EseguiRollMantenendo(this._tieni);
+            else
+                roll = m.EseguiRoll();
+
+            GiriRimanenti.Text = m.Counter.ToString();
+
+
+            PrimaLettera.Text = roll[0].ToString();
+            SecondaLettera.Text = roll[1].ToString();
+            TerzaLettera.Text = roll[2].ToString();
+            Credito.Text = m.SaldoGiocatore.ToString(); ;
+            this._tieni[0] = -1;
+            this._tieni[1] = -1;
+            this._tieni[2] = -1;
+        }
+
+        private void TieniUno(object sender, RoutedEventArgs e)
+        {
+            this._tieni[0] = 0;
+        }
+        private void TieniDue(object sender, RoutedEventArgs e)
+        {
+            this._tieni[1] = 1;
+        }
+        private void TieniTre(object sender, RoutedEventArgs e)
+        {
+            this._tieni[2] = 2;
+        }
+    }
+}
